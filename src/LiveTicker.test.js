52a8ffee1c10e350128c4d7dd6d978d2c8fe65a9
nm357 <n.mikita3@gmail.com>
2017-09-12 15:42:32 -0400

    shallow-render test LiveTicker

--- src/LiveTicker.test.js
+++ src/LiveTicker.test.js
@@ -1,8 +1,13 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
+import { shallow } from 'enzyme';
 import LiveTicker from './LiveTicker';
 
-it('renders without crashing', () => {
+it('full renders without crashing', () => {
   const div = document.createElement('div');
   ReactDOM.render(<LiveTicker />, div);
+});
+
+it('shallow renders without crashing', () => {
+  shallow(<LiveTicker />);
 });
