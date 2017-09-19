c2f2ee262da38ba7706c717c7f80c77e6aab15a6
nm357 <n.mikita3@gmail.com>
2017-09-19 16:30:37 -0400

    LiveTicker initial GDAX websocket trials and debugging

--- src/LiveTicker.js
+++ src/LiveTicker.js
@@ -4,18 +4,41 @@ class LiveTicker extends Component {
   constructor(props){
     super(props);
     this.state = {
-      liveTicker: null
+      liveTicker: null,
+      message: null,
+      error: null
     }
-    this.getTickerPrice = this.getTickerPrice.bind(this);
+    this.openSocket = this.openSocket.bind(this);
   }
-  getTickerPrice = () => {
-    this.setState({liveTicker: this.props.ticker})
-  };
+  openSocket = () => {
+    let gdaxSocket = new WebSocket("wss://ws-feed-public.sandbox.gdax.com");
+    let sendJson = {
+      "type": "subscribe",
+      "channels": [{ "name": "ticker", "product_ids": ["ETH-USD"] }]
+    }
+    gdaxSocket.onopen = () => {
+      gdaxSocket.send(sendJson)
+    }
+    gdaxSocket.onmessage = (event) => {
+      console.log(event.data);
+      this.setState({message: JSON.parse(event.data)})
+      debugger
+    };
+    gdaxSocket.onerror = (error) => {
+      console.log('WebSocket Error ' + error);
+      this.setState({error: error})
+    };
+    gdaxSocket.onclose = () => {
+      gdaxSocket.close()
+    };
+  }
 
   render() {
     return(
       <div>
         <h1>{this.state.liveTicker ? this.state.liveTicker : 'loading'}</h1>
+        <input id='fetch-button' type='button' value='Do WebSocket Thing' onClick={this.openSocket} />
+
       </div>
     )
   }
