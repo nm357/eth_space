e91c0e8523aeb29a44aaf4b62497791b477da041
nm357 <n.mikita3@gmail.com>
2018-01-15 03:27:24 -0500

    LiveTicker and websocket setup

--- src/LiveTicker.js
+++ src/LiveTicker.js
@@ -1,34 +1,55 @@
 import React, { Component } from 'react';
+import Gdax from 'gdax';
+
+const publicClient = new Gdax.PublicClient();
+const subscribeRequest = JSON.stringify({
+  "type": "subscribe",
+  "product_ids": ["ETH-USD"],
+  "channels": [
+    "ticker",
+    "heartbeat"
+  ]
+});
+
+const heartbeatRequest = JSON.stringify({
+    "type": "subscribe",
+    "header":"Access-Control-Allow-Origin",
+    "product_ids": ["ETH-USD"],
+    "channels": [
+      { "name": ["ticker"], product_ids: ["ETH-USD"]},
+      { "name": ["heartbeat"], product_ids: ["ETH-USD"]}
+    ]
+})
 
 class LiveTicker extends Component {
   constructor(props){
     super(props);
     this.state = {
-      liveTicker: null,
+      ticker: null,
+      heartbeat: null,
       message: null,
       error: null
     }
-    this.openSocket = this.openSocket.bind(this);
+    this.openSocket = this.openSocket.bind(this)
+
   }
+
   openSocket = () => {
-    let gdaxSocket = new WebSocket("wss://ws-feed-public.sandbox.gdax.com");
-    let subscribeRequest = JSON.stringify({
-      "type": "subscribe",
-      "product_ids": ["ETH-USD"],
-      "channels": [
-        "ticker"
-      ]
-    });
+    // let gdaxSocket = new WebSocket("wss://ws-feed.gdax.com");
+    let gdaxSocket = new WebSocket("wss://ws-feed.gdax.com")
     gdaxSocket.onopen = () => {
-      gdaxSocket.send(subscribeRequest);
-    }
+      gdaxSocket.send(heartbeatRequest)
+    };
     gdaxSocket.onmessage = (event) => {
-      console.log(event.data);
-      this.setState({message: JSON.parse(event.data)})
+      let data = JSON.parse(event.data)
+      if (data.type === "ticker") {
+        this.setState({ticker: event.data})
+      } else if (data.type === "heartbeat"){
+        this.setState({heartbeat: event.data})
+      }
     };
     gdaxSocket.onerror = (error) => {
-      console.log('WebSocket Error ' + JSON.parse(error));
-      this.setState({error: error})
+      console.log('WebSocket Error ' + error);
     };
     gdaxSocket.onclose = () => {
       gdaxSocket.close()
@@ -38,13 +59,15 @@ class LiveTicker extends Component {
   render() {
     return(
       <div>
-        <h1>{this.state.liveTicker ? this.state.liveTicker : 'loading'}</h1>
+        <div id="heartbeat">Heartbeat: {this.state.heartbeat}</div>
+        <div id="ticker">ETH-USD Ticker: {this.state.ticker}</div>
+        <div>{this.state.liveTicker}</div>
+        <h2>{this.state.message}</h2>
         <input id='fetch-button' type='button' value='Do WebSocket Thing' onClick={this.openSocket} />
-
+        <input id='fetch-button' type='button' value='Stop WebSocket Thing'/>
       </div>
     )
   }
 
 }
-
 export default LiveTicker;
